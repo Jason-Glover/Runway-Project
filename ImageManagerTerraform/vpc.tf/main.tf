2900109955b8c85b804c20508dc35f8d6abee1d2
JasonGlover <jason.glover@rackspace.com>
2021-10-05 10:38:06 -0500

    code change for runway

--- ImageManagerTerraform/vpc.tf/main.tf
+++ ImageManagerTerraform/vpc.tf/main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "${var.region}"
 }
 
 # Declare the data source to obtain available AZ's
--- ImageManagerTerraform/vpc.tf/variables.tf
+++ ImageManagerTerraform/vpc.tf/variables.tf
@@ -1,7 +1,6 @@
-variable "aws_region" {
+variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "environment" {
