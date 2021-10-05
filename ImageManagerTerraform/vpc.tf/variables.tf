c02c0c2fcf87fe34e97d3c660aee9646b2005fd8
JasonGlover <jason.glover@rackspace.com>
2021-10-05 01:51:26 -0500

    new project

---
+++ ImageManagerTerraform/vpc.tf/variables.tf
@@ -0,0 +1,47 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  type        = string
+  default     = "common"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.132.0.0/16"
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets."
+  type        = number
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets."
+  type        = number
+  default     = 2
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "Available cidr blocks for public subnets."
+  type        = list(string)
+  default     = [
+    "10.132.120.0/24",
+    "10.132.121.0/24",
+   ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets."
+  type        = list(string)
+  default     = [
+    "10.132.122.0/24",
+    "10.132.123.0/24",
+  ]
+}
+
